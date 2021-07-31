28fc0d45bb12d3f7a8e35ac2de5e88b3f2617879
Snowflakes626 <951316647@qq.com>
2021-07-31 13:13:11 +0800

    分支测试

--- test/src/main/java/com/sf/test/mytest.java
+++ test/src/main/java/com/sf/test/mytest.java
@@ -1,7 +1,10 @@
 package com.sf.test;
 
+import jdk.internal.org.objectweb.asm.tree.analysis.SourceValue;
+
 public class mytest {
     public static void main(String[] args) {
         System.out.println("你好！");
+        System.out.println("测试");
     }
 }
